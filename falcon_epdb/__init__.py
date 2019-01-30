0c6f1144bde369a8a53623c88bf6fd5f74860880
Josh Wilson <josh.wilson@fivestars.com>
2019-01-30 15:00:05 -0800

    Adapt to pydocstyle

--- falcon_epdb/__init__.py
+++ falcon_epdb/__init__.py
@@ -1,3 +1,5 @@
+"""Remote debugging support for Falcon apps."""
+
 __version__ = '0.1.0'
 
 import json
@@ -21,37 +23,38 @@ logger = getLogger(__name__)
 
 
 class EPDBException(Exception):
-    """ Raised when an error occurs during the processing of an ``X-EPDB`` header. """
+    """Raised when an error occurs during the processing of an ``X-EPDB`` header."""
 
 class EPDBServe(object):
-    """ A middleware to enable remote debuging via an `epdb`_ server.
+    """A middleware to enable remote debuging via an `epdb`_ server.
 
-        :param backend: An instance of the class that will validate and decode the ``X-EPDB`` header
-        :param exempt_methods: HTTP methods which will be ignored by this middleware
-        :param serve_options: Parameters passed-through to :func:`epdb.serve()`
-        :type backend: EPDBBackend
-        :type exempt_methods: iterable of strings
-        :type serve_options: dictionary
+    :param backend: An instance of the class that will validate and decode the ``X-EPDB`` header
+    :param exempt_methods: HTTP methods which will be ignored by this middleware
+    :param serve_options: Parameters passed-through to :func:`epdb.serve()`
+    :type backend: EPDBBackend
+    :type exempt_methods: iterable of strings
+    :type serve_options: dictionary
 
-        A client may include a special ``X-EPDB`` header containing an appropriately formed payload.
-        If they do, the header will be passed to the configured backend for processing. If the
-        payload passes authentication and meets the content requirements, the app will be begin
-        listening for `epdb`_ client connections.
+    A client may include a special ``X-EPDB`` header containing an appropriately formed payload.
+    If they do, the header will be passed to the configured backend for processing. If the
+    payload passes authentication and meets the content requirements, the app will be begin
+    listening for `epdb`_ client connections.
 
-        A well-formed header has content simply of the form::
+    A well-formed header has content simply of the form::
 
-            {
-                "epdb": {
-                    # optional
-                    "port": <port value>
-                }
+        {
+            "epdb": {
+                # optional
+                "port": <port value>
             }
+        }
 
-        The encoding and encryption of this payload is determined by the :class:`EPDBBackend`
-        provided to the middleware.
+    The encoding and encryption of this payload is determined by the :class:`EPDBBackend`
+    provided to the middleware.
 
-        .. _epdb: https://pypi.org/project/epdb/
+    .. _epdb: https://pypi.org/project/epdb/
     """
+
     def __init__(self, backend, exempt_methods=('OPTIONS',), serve_options=None):
         serve_options = serve_options or {}
         self.backend = backend
@@ -59,16 +62,16 @@ class EPDBServe(object):
         self.serve_options = serve_options
 
     def process_request(self, req, resp):
-        """ Check for a well-formed ``X-EPDB`` header and if present activate the `epdb`_ server.
+        """Check for a well-formed ``X-EPDB`` header and if present activate the `epdb`_ server.
 
-            :param req: The Falcon request object
-            :param resp: The Falcon response object (unused)
+        :param req: The Falcon request object
+        :param resp: The Falcon response object (unused)
 
-            This will block, waiting for an `epdb`_ client connection, the first time a valid
-            header is received. Once the client is connected, subsequent passes will simply activate
-            the connected client and drop it into the `epdb`_ shell.
+        This will block, waiting for an `epdb`_ client connection, the first time a valid
+        header is received. Once the client is connected, subsequent passes will simply activate
+        the connected client and drop it into the `epdb`_ shell.
 
-            The header processing is delegated to the configured :class:`EPDBBackend`.
+        The header processing is delegated to the configured :class:`EPDBBackend`.
         """
         if req.method in self.exempt_methods:
             return
@@ -89,26 +92,27 @@ class EPDBServe(object):
 
 
 class EPDBBackend(object):
-    """ The abstract base class defining the header-processing backend interface.
+    """The abstract base class defining the header-processing backend interface.
 
-        An inheriting subclass must define :meth:`decode_header_value`, but may define other methods
-        if necessary. This class is structured to provide a balance of convenience and flexibility.
+    An inheriting subclass must define :meth:`decode_header_value`, but may define other methods
+    if necessary. This class is structured to provide a balance of convenience and flexibility.
     """
+
     __metaclass__ = ABCMeta
 
     def get_header_data(self, req):
-        """ Process a request and return the contents of a conforming payload.
+        """Process a request and return the contents of a conforming payload.
 
-            :param req: The Falcon request object
-            :type req: Request
-            :returns: The paylod content or None
-            :rtype: dictionary or None
+        :param req: The Falcon request object
+        :type req: Request
+        :returns: The paylod content or None
+        :rtype: dictionary or None
 
-            This implementation assumes that the payload is present on the ``X-EPDB`` header, but
-            subclasses may override this method if their use-case demands it.
+        This implementation assumes that the payload is present on the ``X-EPDB`` header, but
+        subclasses may override this method if their use-case demands it.
 
-            If the request does not appear to be attempting begin a debugging session, this will
-            return :obj:`None`.
+        If the request does not appear to be attempting begin a debugging session, this will
+        return :obj:`None`.
         """
         epdb_header = req.headers.get('X-EPDB')
         if epdb_header:
@@ -118,35 +122,35 @@ class EPDBBackend(object):
 
     @abstractmethod
     def decode_header_value(self, epdb_header):
-        """ Process the ``X-EPDB`` header content.
+        """Process the ``X-EPDB`` header content.
 
-            :param epdb_header: The content of the ``X-EPDB`` header
-            :type epdb_header: string
-            :returns: The decoded and decrypt header payload
-            :rtype: dictionary
+        :param epdb_header: The content of the ``X-EPDB`` header
+        :type epdb_header: string
+        :returns: The decoded and decrypt header payload
+        :rtype: dictionary
 
-            This does not need to do any content validation, as that is handled in
-            :meth:`validate_header_content`.
+        This does not need to do any content validation, as that is handled in
+        :meth:`validate_header_content`.
         """
         pass
 
     def validate_header_content(self, header_content):
-        """ Ensure that the decoded ``X-EPDB`` header content is well-formed.
+        """Ensure that the decoded ``X-EPDB`` header content is well-formed.
 
-            :param header_content: The decoded ``X-EPDB`` header content
-            :type header_content: dictionary
-            :returns: The value of the `epdb`_ key
-            :rtype: dictionary
-            :raises: EPDBException
+        :param header_content: The decoded ``X-EPDB`` header content
+        :type header_content: dictionary
+        :returns: The value of the `epdb`_ key
+        :rtype: dictionary
+        :raises: EPDBException
 
-            :obj:`header_content` must be of the form::
+        :obj:`header_content` must be of the form::
 
-                {
-                    "epdb": {
-                        # optional
-                        "port": <port value>
-                    }
+            {
+                "epdb": {
+                    # optional
+                    "port": <port value>
                 }
+            }
         """
         if not isinstance(header_content, dict):
             raise EPDBException('Invalid X-EPDB content; must be a dictionary')
@@ -161,32 +165,33 @@ class EPDBBackend(object):
 
 
 class FernetBackend(EPDBBackend):
-    """ A Python cryptography-based backend that supports a pre-shared key (ie. password) protocol.
+    """A Python cryptography-based backend that supports a pre-shared key (ie. password) protocol.
 
-        :param key: The fernet key used to encrypt the header content
-        :type key: bytes
+    :param key: The fernet key used to encrypt the header content
+    :type key: bytes
 
-        To generate the request header, one may do something like the following:
+    To generate the request header, one may do something like the following:
 
-        .. code-block:: python
+    .. code-block:: python
 
-            import json
-            from cryptography.fernet import Fernet
+        import json
+        from cryptography.fernet import Fernet
 
-            key = Fernet.generate_key()
-            f = Fernet(key)
-            header_content = f.encrypt(json.dumps({'epdb': {}}).encode()).decode()
-            header_value = 'Fernet {}'.format(header_content)
+        key = Fernet.generate_key()
+        f = Fernet(key)
+        header_content = f.encrypt(json.dumps({'epdb': {}}).encode()).decode()
+        header_value = 'Fernet {}'.format(header_content)
 
-        .. note:: To use this backend, one must install the :mod:`cryptography` package. The easiest
-            way to do this is to specify the ``[fernet]`` extra when adding the ``falcon-epdb``
-            dependency to your project.
+    .. note:: To use this backend, one must install the :mod:`cryptography` package. The easiest
+        way to do this is to specify the ``[fernet]`` extra when adding the ``falcon-epdb``
+        dependency to your project.
 
-            .. code-block:: text
-                :caption: **requirements.txt**
+        .. code-block:: text
+            :caption: **requirements.txt**
 
-                falcon-epdb[fernet]
+            falcon-epdb[fernet]
     """
+
     def __init__(self, key):
         try:
             self.fernet = fernet.Fernet(key)
@@ -194,15 +199,15 @@ class FernetBackend(EPDBBackend):
             raise ImportError('Missing optional [fernet] dependency')
 
     def decode_header_value(self, epdb_header):
-        """ Pull the encrypted data out of the header, if present.
+        """Pull the encrypted data out of the header, if present.
 
-            :param epdb_header: The content of the ``X-EPDB`` header.
-            :type epdb_header: string
-            :returns: The decoded and decrypt header payload
-            :rtype: dictionary
-            :raises: EPDBException
+        :param epdb_header: The content of the ``X-EPDB`` header.
+        :type epdb_header: string
+        :returns: The decoded and decrypt header payload
+        :rtype: dictionary
+        :raises: EPDBException
 
-            It expects :obj:`epdb_header` to have the ``Fernet`` prefix.
+        It expects :obj:`epdb_header` to have the ``Fernet`` prefix.
         """
         try:
             scheme, payload = epdb_header.split(None, 1)
@@ -218,30 +223,30 @@ class FernetBackend(EPDBBackend):
 
 
 class JWTBackend(EPDBBackend):
-    """ A JWT-based backend that supports a pre-shared key (ie. password) protocol.
+    """A JWT-based backend that supports a pre-shared key (ie. password) protocol.
 
-        :param key: The JWT key used to encrypt the header content
-        :type key: bytes
+    :param key: The JWT key used to encrypt the header content
+    :type key: bytes
 
-        To generate the request header, one may do something like the following:
+    To generate the request header, one may do something like the following:
 
-        .. code-block:: python
+    .. code-block:: python
 
-            import jwt
-            import uuid
+        import jwt
+        import uuid
 
-            key = uuid.uuid4().hex
-            header_content = jwt.encode({'epdb': {}}, key, algorithm='HS256').decode()
-            header_value = 'JWT {}'.format(header_content)
+        key = uuid.uuid4().hex
+        header_content = jwt.encode({'epdb': {}}, key, algorithm='HS256').decode()
+        header_value = 'JWT {}'.format(header_content)
 
-        .. note:: To use this backend, one must install the :mod:`PyJWT` package. The easiest
-            way to do this is to specify the ``[jwt]`` extra when adding the ``falcon-epdb``
-            dependency to your project.
+    .. note:: To use this backend, one must install the :mod:`PyJWT` package. The easiest
+        way to do this is to specify the ``[jwt]`` extra when adding the ``falcon-epdb``
+        dependency to your project.
 
-            .. code-block:: text
-                :caption: **requirements.txt**
+        .. code-block:: text
+            :caption: **requirements.txt**
 
-                falcon-epdb[jwt]
+            falcon-epdb[jwt]
     """
 
     def __init__(self, key):
@@ -253,15 +258,15 @@ class JWTBackend(EPDBBackend):
         self.key = key
 
     def decode_header_value(self, epdb_header):
-        """ Pull the encrypted data out of the header, if present.
+        """Pull the encrypted data out of the header, if present.
 
-            :param epdb_header: The content of the ``X-EPDB`` header.
-            :type epdb_header: string
-            :returns: The decoded and decrypt header payload
-            :rtype: dictionary
-            :raises: EPDBException
+        :param epdb_header: The content of the ``X-EPDB`` header.
+        :type epdb_header: string
+        :returns: The decoded and decrypt header payload
+        :rtype: dictionary
+        :raises: EPDBException
 
-            It expects :obj:`epdb_header` to have the ``JWT`` prefix.
+        It expects :obj:`epdb_header` to have the ``JWT`` prefix.
         """
         try:
             scheme, payload = epdb_header.split(None, 1)
